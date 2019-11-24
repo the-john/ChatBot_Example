84027076961cb25723331580d517347284bd0858
Ubuntu_18.04.3 <delton.oliver@outlook.com>
2019-11-23 19:22:40 -0800

    working commit

---
+++ src/chatbot.h
@@ -0,0 +1,47 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();                                                                     // Destructor
+
+                                                                                    // ************************* Task 2. Rule of Five. ***********************
+    ChatBot(const ChatBot &source);                                                 // Copy Constructor
+    ChatBot &operator=(const ChatBot &source);                                      // Copy Assignment
+    ChatBot(ChatBot &&source);                                                      // Move Constructor
+    ChatBot &operator=(ChatBot &&source);                                           // Move Assignment
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+    ChatLogic *GetChatLogicHandle() { return this -> _chatLogic; }                  // ****************
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
---
+++ src/graphnode.cpp
@@ -0,0 +1,51 @@
+#include "graphedge.h"
+#include "graphnode.h"
+#include <iostream>                         // for debugging
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    // delete _chatBot;                     // ****************** Task 0.  This is what makes the original code crash when shut-down. ***********************
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)             // *************************** Task 3. ***************************************
+{
+    _childEdges.push_back(std::move(edge));                                     // modify for exclusive ownership
+}
+
+void GraphNode::MoveChatbotHere(ChatBot chatbot)                                // ************************** Task 3. ****************************************
+{
+    _chatBot = std::move(chatbot);                                              // modify for exclusive ownership
+    _chatBot.SetCurrentNode(this);                                              // cont.
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    ChatBot movedchatbot = ChatBot("../images/chatbot.png");                    // *************************** Task 4. ***************************************
+    newNode -> MoveChatbotHere(std::move(_chatBot));                            // *************************** Task 4. ***************************************
+}
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    return _childEdges[index].get();                                            // ****************************** Task 3. ************************************
+}
